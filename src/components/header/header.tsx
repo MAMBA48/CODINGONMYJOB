364a88f2ee0ebef3c810c638627c76bb1a0ed4c7
MAMBA48 <61758608+MAMBA48@users.noreply.github.com>
2023-09-06 14:44:37 -0300

    1 file modified

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,17 +1,22 @@
 import { useState } from 'react';
 const Header = () => {
   //testando
-  const [content, setContent] = useState<string>('');
+  const [content, setContent] = useState<string>('home');
   const myProducts = () => {
     setContent('products');
+  };
+  const myContact = () => {
+    setContent('contact');
   };
   return (
     <header>
       <h2>Header</h2>
       <nav>
         <button onClick={myProducts}>Produtos</button>
-        <button>Contato</button>
+        <button onClick={myContact}>Contato</button>
       </nav>
+      {content === 'products' && <h2>AREA OF PRODUCTS</h2>}
+      {content === 'contact' && <h2>AREA OF CONTACT</h2>}
     </header>
   );
 };
