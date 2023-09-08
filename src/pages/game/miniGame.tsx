e901a785d761221b594fb8d335cbae5d21eac3ba
MAMBA48 <61758608+MAMBA48@users.noreply.github.com>
2023-09-08 11:57:41 -0300

    1 file modified

--- src/pages/game/miniGame.tsx
+++ src/pages/game/miniGame.tsx
@@ -1,3 +1,14 @@
+import { useState } from 'react';
+import { styled } from 'styled-components';
+
+const Span = styled.span`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border: 2px solid #000;
+  padding: 30px;
+`;
+
 const Game = () => {
   const date = new Date();
   const hour = date.getHours();
@@ -5,20 +16,21 @@ const Game = () => {
   const sec = date.getSeconds();
   const time = `${hour}:${min}:${sec}`;
 
-  const play = () => {
-    return (
-      <>
-        <p>beginning</p>
-      </>
-    );
-  };
+  const [play, setPlay] = useState(false);
+  const playGame = () => setPlay(true);
+
   return (
     <>
       <h2>Let's play a game?</h2>
       <article>
         <h3>If you ready click on the button:</h3>
-        <button onClick={play}>Play</button>
-        {setInterval(() => sec, 1000)}
+        <button onClick={playGame}>Play</button>
+        {play && (
+          <Span>
+            <p>{time}</p>
+            <h4>voce conhece o continente onde habita?</h4>
+          </Span>
+        )}
       </article>
     </>
   );
